0bc46cf283c572924d8f1fb343e0a57b3fc69390
ASefedin <115707837+ASefedin@users.noreply.github.com>
2023-12-08 13:43:51 +0100

    Add files via upload
    le code de tom

---
+++ BasicGym/index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php require_once "Views/Components/navBar.php"; ?>
+    </header>
+    <main>
+
+    </main>
+    <flooter>
+        <?php require_once "Views/Components/footer.php"; ?>
+    </flooter>
+</body>
+</html>
