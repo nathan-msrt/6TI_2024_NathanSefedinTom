54d36842f0a94617d7f26c1768e3c5f26444c50a
Massart Nathan <220747@CCM-Asty-Moulin.local>
2024-04-23 16:11:01 +0200

    test push

--- BasicGym/Views/Components/OurProgram.php
+++ BasicGym/Views/Components/OurProgram.php
@@ -1,11 +1,12 @@
-<img class="imgFond" src="img/FitnessVert.jpg" alt="FitnessVert.jpg">
-<div>
-    <div class="flex">
-        <div class="divCompte">
-            <H1>Votre compte</H1>
-        </div>
-        <div class="divCritere">
-            <h1>Vos critères</h1>
+<div class="divImgFond">
+    <div>
+        <div class="flex">
+            <div class="divCompte">
+                <H1>Votre compte</H1>
+            </div>
+            <div class="divCritere">
+                <h1>Vos critères</h1>
+            </div>
         </div>
     </div>
 </div>
