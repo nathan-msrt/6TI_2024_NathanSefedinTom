b18df70b27eab43324f6fb185273425b185d4e9c
Massart Nathan <220747@CCM-Asty-Moulin.local>
2024-05-24 14:17:15 +0200

    echange

--- BasicGym/Views/Components/CreateProgram.php
+++ BasicGym/Views/Components/CreateProgram.php
@@ -6,14 +6,18 @@
             <fieldset>
                         <legend>Créer votre Programme</legend>
                         
+                            <div>
                             <label for="gender">Sexe:</label >
                                 <select id="sexe" name="sexe" value="<?php if(isset($_SESSION["critereutilisateur"])) : ?><?= $_SESSION["critereutilisateur"]->critereUtilisateurSexe ?><?php endif ?>" required>
                                     <option value="male">Homme</option>
                                     <option value="female">Femme</option>
                                 </select>
+                            </div>
 
+                        <div>
                         <label for="age">Âge:</label>
                         <input type="number" id="age" name="age" value="<?php if(isset($_SESSION["critereutilisateur"])) : ?><?= $_SESSION["critereutilisateur"]->critereUtilisateurAge ?><?php endif ?>" required>
+                        </div>
 
                     <div>
                         <label for="height">Taille (en cm):</label>
