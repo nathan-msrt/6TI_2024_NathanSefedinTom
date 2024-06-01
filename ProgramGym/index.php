d94529cacfefc18bd47bd948750827f514c7a4fd
TomRij <200082@site.asty-moulin.be>
2024-06-02 00:21:00 +0200

    rename of the folder BasycGym to ProgramGym

---
+++ ProgramGym/index.php
@@ -0,0 +1,10 @@
+
+<?php
+    session_start();
+    require_once __DIR__ . "/Config/databaseConnexion.php";
+    require_once __DIR__ . "/Controllers/indexController.php";
+    require_once __DIR__ . "/Controllers/userController.php";
+    require_once __DIR__ . "/Controllers/critereUtilisateurController.php";
+    require_once __DIR__ . "/Controllers/genProgUserController.php";
+
+
